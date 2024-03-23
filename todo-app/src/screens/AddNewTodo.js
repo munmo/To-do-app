18b83998730777cdd6d69bf410f5a9cd281f82c7
Sonya Jung <sonya@192-168-1-105.tpgi.com.au>
2024-03-23 11:43:02 +1000

    Created text input - milestone2

--- todo-app/src/screens/AddNewTodo.js
+++ todo-app/src/screens/AddNewTodo.js
@@ -1,13 +1,38 @@
-import {Text, View, StyleSheet} from "react-native";
+import {View, StyleSheet} from "react-native";
+import * as React from 'react';
+import { Text } from 'react-native-paper';
+import { TextInput } from 'react-native-paper';
+import {useState} from 'react';
 
 export default function AddNewTodo () {
-    return (
-        <View>
-            <Text>Add New Todo</Text>
-        </View>
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Text variant="headlineSmall">Title</Text>
+      <TextInput
+        value={title}
+        onChangeText={text => setTitle(text)}
+      />
+      <Text variant="headlineSmall">Description</Text>
+      <TextInput
+        value={description}
+        onChangeText={text => setDescription(text)}
+        multiline
+        numberOfLines={4}
+      />
+    </View>
     )
 }
 
 const styles = StyleSheet.create (
-{}
+{
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingHorizontal: 20,
+      },
+}
 )
